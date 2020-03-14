184b4b5494c9ae9040b9f82b4c647e1393a7ce4e
brodny <brodny@wp.pl>
2020-03-14 23:25:08 +0100

    Unit test project added

---
+++ SampleLibrary.Tests/GreetingGeneratorTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace SampleLibrary.Tests
+{
+    public class GreetingGeneratorTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
