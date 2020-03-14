a03b51c5dd2dff6f6552414006932b1a1cbd26c7
brodny <brodny@wp.pl>
2020-03-14 23:15:10 +0100

    ConsoleApp uses SampleLibrary

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SampleLibrary;
 
 namespace SampleApp
 {
@@ -6,7 +7,13 @@ namespace SampleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string myName = "Lukasz";
+
+            GreetingGenerator generator = new GreetingGenerator();
+
+            string greeting = generator.GenerateGreeting(myName);
+
+            Console.WriteLine(greeting);
         }
     }
 }
